d6e875ecb9d7c5285465e11f905a31a99a062002
824zzy <614594368@qq.com>
2020-06-27 16:47:32 -0500

    refactor($hw3):

--- hw3/application.py
+++ hw3/application.py
@@ -113,7 +113,6 @@ def insert():
     status = request.args.get('status') if request.args.get('status') else -1
     locsource = request.args.get('locsource') if request.args.get('locsource') else -1
     magsource = request.args.get('magsource') if request.args.get('magsource') else -1
-    print('bb', magsource, 'aa', request.args.get('magsource'))
     start = time.time()
     # connect to DB2
     if mode=='RDB':
@@ -125,7 +124,6 @@ def insert():
                     PLACE, TYPE, HOTIZONTALERROR, DEPTHERROR, \
                     MAGERROR, MAGNST, STATUS, LOCATIONSOURCE, MAGSOURCE) \
                     VALUES ({}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {});""".format(etime, lat, lon, dep, mag, magtype, nst, gap, dmin, rms, net, eid, updated, place, etype, he, de, me, mn, status, locsource, magsource)
-            print(sql)
             ibm_db.exec_immediate(db2conn, sql)
     elif mode=='Memcache':
         tmp = pd.DataFrame([[etime, float(lat), float(lon), float(dep), float(mag), magtype, float(nst), float(gap), float(dmin), float(rms), net, eid, updated, place, etype, float(he), float(de), float(me), float(mn), status, locsource, magsource]])
@@ -143,6 +141,7 @@ def insert():
 def delete():
     mode = request.args.get('dmode1') if request.args.get('dmode1') else request.args.get('dmode2')
     mode = mode.split(' ')[-1]
+    start = time.time()
     
     etime = request.args.get('time') if request.args.get('time') else 'None'
     lat = request.args.get('lat') if request.args.get('lat') else -1 
@@ -196,13 +195,13 @@ def delete():
     if mode=='RDB':
         # connect to DB2
         db2conn = ibm_db.connect(db2cred['ssldsn'], "","")
-        start = time.time()
         if db2conn:
             sql = """DELETE FROM EARTHQUAKE WHERE {}""".format(sub_sql)
             ibm_db.exec_immediate(db2conn, sql)
     elif mode=='Memcache':
-        # TODO:
-        pass
+        for p in params:
+            global cache_csv
+            cache_csv = cache_csv[cache_csv[p[0]]!=p[1]]
         
     end = time.time()
     elapse = end - start    
@@ -279,151 +278,6 @@ def search_around_place():
     end = time.time()
     elapse = end - start
     return render_template('search_around_place.html', ci=rows, elapse=elapse)
-
-
-
-# @app.route('/count_scale', methods=['GET'])
-# def count_scale():
-#     st = time.time()
-#     start = request.args.get('start', default='2020-06-01')
-#     end = request.args.get('end', default='2020-06-01')
-#     start = '2020-06-01' if start=='' else start
-#     end = '2020-06-08' if end=='' else end
-#     start = datetime.datetime.strptime(start, "%Y-%m-%d")
-#     end = datetime.datetime.strptime(end, "%Y-%m-%d")
-#     scale = request.args.get('scale', '3')
-
-#     # connect to DB2
-#     db2conn = ibm_db.connect(db2cred['ssldsn'], "","")
-#     if db2conn:
-#         sql = "SELECT * FROM EARTHQUAKE WHERE MAGTYPE=\'ml\' AND MAG>=?"
-#         stmt = ibm_db.prepare(db2conn, sql)
-#         ibm_db.bind_param(stmt, 1, scale)
-#         ibm_db.execute(stmt)
-        
-#         rows=[]
-#         result = ibm_db.fetch_assoc(stmt)
-#         while result != False:
-#             curr_date = result['TIME'][:10]
-#             curr_date = datetime.datetime.strptime(curr_date, "%Y-%m-%d")
-#             if start<=curr_date<=end:
-#                 rows.append(result.copy())
-#             result = ibm_db.fetch_assoc(stmt)
-    
-#         ibm_db.close(db2conn)
-    
-#     et = time.time()
-#     elapse = et - st
-#     return render_template('count_scale.html', ci=rows, elapse=elapse)
-
-# @app.route('/search_scale', methods=['GET'])
-# def search_scale():
-#     start = time.time()
-#     number = request.args.get('number', 1000, type=int)
-#     today = datetime.date.today()
-#     ago = today - datetime.timedelta(days=number)
-    
-#     slot12, slot23, slot34, slot45, slot56, slot67 = 0, 0, 0, 0, 0, 0
-#     # connect to DB2
-#     db2conn = ibm_db.connect(db2cred['ssldsn'], "","")
-#     if db2conn:
-#         sql = "SELECT * FROM EARTHQUAKE WHERE MAGTYPE=\'ml\'"
-#         stmt = ibm_db.exec_immediate(db2conn, sql)
-#         # fetch the result
-#         result = ibm_db.fetch_assoc(stmt)
-#         while result != False:
-#             curr_date = result['TIME'][:10]
-#             curr_date = datetime.datetime.strptime(curr_date, "%Y-%m-%d")
-#             if curr_date.date()>=ago:
-#                 print(curr_date)
-#                 mag_scale = float(result['MAG'])
-#                 if 1<=mag_scale<=2:
-#                     slot12 += 1
-#                 if 2<=mag_scale<=3:
-#                     slot23 += 1
-#                 if 3<=mag_scale<=4:
-#                     slot34 += 1
-#                 if 4<=mag_scale<=5:
-#                     slot45 += 1
-#                 if 5<=mag_scale<=6:
-#                     slot56 += 1
-#                 if 6<=mag_scale<=7:
-#                     slot67 += 1
-#             result = ibm_db.fetch_assoc(stmt)
-#         # close database connection
-#         ibm_db.close(db2conn)
-#     end = time.time()
-#     elapse = end - start
-#     return render_template('search_scale.html', ci=[slot12, slot23, slot34, slot45, slot56, slot67], elapse=elapse)
-
-
-
-# @app.route('/compare_two_place', methods=['GET'])
-# def compare_two_place():
-#     start = time.time()
-#     distance = request.args.get('distance', 1000, type=int)
-    
-#     placeA, placeB = request.args.get('placeA'), request.args.get('placeB')
-#     placeA = 'Anchorage' if placeA=='' else placeA
-#     placeB = 'Dallas' if placeB=='' else placeB
-    
-#     pA_json, pB_json = geocoder.osm(placeA).json, geocoder.osm(placeB).json
-#     trgtA_coords, trgtB_coords = (pA_json['lat'], pA_json['lng']), (pB_json['lat'], pB_json['lng'])
-
-#     # connect to DB2
-#     db2conn = ibm_db.connect(db2cred['ssldsn'], "","")
-#     if db2conn:
-#         sql = "SELECT * FROM EARTHQUAKE"
-#         stmt = ibm_db.exec_immediate(db2conn, sql)
-#         ansA, ansB = [], []
-#         # fetch the result
-#         result = ibm_db.fetch_assoc(stmt)
-#         while result != False:
-#             curr_coords = (result['LATITUDE'], result['LONGTITUDE'])
-#             if geopy.distance.vincenty(curr_coords, trgtA_coords).km<distance:
-#                 ansA.append(result.copy())
-#             if geopy.distance.vincenty(curr_coords, trgtB_coords).km<distance:
-#                 ansB.append(result.copy())
-#             result = ibm_db.fetch_assoc(stmt)
-#         # close database connection
-#         ibm_db.close(db2conn)
-    
-#     end = time.time()
-#     elapse = end - start
-#     return render_template('compare_two_place.html', ciA=ansA, ciB=ansB, pA=placeA, pB=placeB, elapse=elapse)
-
-
-
-# @app.route('/largest_around_place', methods=['GET'])
-# def largest_around_place():
-#     start = time.time()
-#     distance = request.args.get('distance', 500, type=int)
-#     city = request.args.get('city')
-#     city = 'Dallas' if city=='' else city
-#     usr_g_json = geocoder.osm(city).json
-#     trgt_coords = (usr_g_json['lat'], usr_g_json['lng'])
-
-#     # connect to DB2
-#     db2conn = ibm_db.connect(db2cred['ssldsn'], "","")
-#     if db2conn:
-#         sql = "SELECT * FROM EARTHQUAKE"
-#         stmt = ibm_db.exec_immediate(db2conn, sql)
-#         ans, largest = [], 0
-#         # fetch the result
-#         result = ibm_db.fetch_assoc(stmt)
-#         while result != False:
-#             curr_coords = (result['LATITUDE'], result['LONGTITUDE'])
-#             if geopy.distance.vincenty(curr_coords, trgt_coords).km<distance and float(result['MAG'])>largest:
-#                 largest = float(result['MAG'])
-#                 ans = [result.copy()]
-#             result = ibm_db.fetch_assoc(stmt)
-#         # close database connection
-#         ibm_db.close(db2conn)
-    
-#     end = time.time()
-#     elapse = end - start
-#     return render_template('largest_around_place.html', ci=ans, elapse=elapse)
-
 
 
 port = os.getenv('PORT', '5000')
